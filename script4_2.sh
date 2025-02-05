230b91049666acf69641badda3ad35e0ad54b428
ytrqua3 <suensenho@gmail.com>
2025-02-05 01:47:16 +0000

    question2 updated not finished

---
+++ script4_2.sh
@@ -0,0 +1,18 @@
+suspect=$1
+processed=$2
+temp=$(mktemp)
+sid=$(mktemp)
+grade=$(mktemp)
+process_temp=$(mktemp)
+tail -n +2 $suspect | cut -d"," -f1,5 | sort -t"," -k1 >$temp
+tail -n +2 $processed | sort -t"," -k2 >$process_temp
+cut -d"," -f1 $process_temp >$grade
+cut -d"," -f2 $process_temp >$sid
+paste -d"," $sid $grade >$process_temp
+
+comm $process_temp $temp
+
+rm $temp
+rm $sid
+rm $grade
+rm $process_temp
