596eefb7e134ef22f515932ae69691f8e0488bc7
ytrqua3 <suensenho@gmail.com>
2025-02-06 01:36:24 +0000

    all scripts passed tests

--- script4_3.sh
+++ script4_3.sh
@@ -1,1 +1,15 @@
+process_ids=$(mktemp)
+info=$(mktemp)
+fnames=$(mktemp)
+ps -f --ppid $1 | tr -s ' ' >$info 
 
+cut -d' ' -f2 <$info| tail -n +2 >$process_ids
+cut -d' ' -f8,9,10 $info | tail -n +2 >$fnames
+
+paste -d',' $process_ids $fnames
+
+cat $process_ids | xargs -I{} kill {}
+
+rm $process_ids
+rm $info
+rm $fnames
